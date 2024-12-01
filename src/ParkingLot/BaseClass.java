68159c3035a4f81195389a0e52a7df07af664945
arshadcp <arshadcp055@gmail.com>
2024-12-02 01:41:50 +0530

    baseClass

---
+++ src/ParkingLot/BaseClass.java
@@ -0,0 +1,61 @@
+package ParkingLot;
+import java.time.*;
+public abstract class BaseClass {
+    int id;
+  private  LocalDateTime createdAt;
+  private  LocalDateTime updatedAt;
+  private  LocalDateTime createdBy;
+  private  LocalDateTime updatedBy;
+
+    public BaseClass(int id, LocalDateTime CreatedAt, LocalDateTime UpdatedAt, LocalDateTime CreatedBy,LocalDateTime UpdatedBy) {
+        this.id = id;
+        createdAt = CreatedAt;
+        updatedAt = UpdatedAt;
+        createdBy = CreatedBy;
+       updatedBy = UpdatedBy;
+    }
+
+    protected BaseClass(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime CreatedAt) {
+        createdAt = CreatedAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime UpdatedAt) {
+       updatedAt = UpdatedAt;
+    }
+
+    public LocalDateTime getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(LocalDateTime CreatedBy) {
+       createdBy = CreatedBy;
+    }
+
+    public LocalDateTime getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(LocalDateTime UpdatedBy) {
+        updatedBy = UpdatedBy;
+    }
+}
